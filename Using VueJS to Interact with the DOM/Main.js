6e5d04ecfe54a3fc8dcb436d6ca23b3c5fd67e27
Arjun ghimire <developerarjun1@gmail.com>
2020-05-05 22:28:45 +0545

    interact to DOM

---
+++ Using VueJS to Interact with the DOM/Main.js
@@ -0,0 +1,28 @@
+new Vue({
+    el: '#app',
+    data: {
+        title: "Hello!!",
+        link: "http://google.com",
+        finishedLink: '<a href="http://google.com">Google</a>',
+        counter: 0,
+        x: 0,
+        y: 0
+    },
+    methods: {
+        sayHello: function(){
+            this.title = "Hello World";
+            return this.title;
+        },
+        increaseCounter: function(step,event){
+            console.log(event);
+            this.counter += step;
+        },
+        updateCoordinates: function(event){
+            this.x=event.clientX;
+            this.y=event.clientY;
+        },
+        alertMe : function(){
+            alert("Alert");
+        }
+    }
+});
