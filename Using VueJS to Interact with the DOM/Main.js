fc0db8d29c7dee5cb87d420bdd317e8169d3969e
Arjun ghimire <developerarjun1@gmail.com>
2020-05-06 18:17:43 +0545

    dynamically css

--- Using VueJS to Interact with the DOM/Main.js
+++ Using VueJS to Interact with the DOM/Main.js
@@ -8,13 +8,37 @@ new Vue({
         x: 0,
         y: 0,
         name: "Arjun",
-        secondCounter: 0
+        secondCounter: 0,
+        attachedRed: false,
+        attachedGreen: false,
+        color: "blue",
+        colorProperty: "black",
+        width: 100,
     },
     computed:{
         output : function(){
             console.log("Computed");
             return this.counter > 10 ? "Greater than 10": "Lower than 10";
+        },
+        divClass: function(){
+            return{
+                red: this.attachedRed
+            }
+        },
+        myStyle: function(){
+            return {
+                backgroundColor: this.colorProperty,
+                width: this.width + 'px'
+            }
         }
+    },
+    watch: {
+      counter: function(){
+          var vm = this;
+          setTimeout(function(){
+            vm.counter = 0;
+          },2000)
+      }  
     },
     methods: {
         sayHello: function(){
