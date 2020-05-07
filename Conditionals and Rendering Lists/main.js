38484992b812915dbc00ffde97d0603fe992471d
Arjun ghimire <developerarjun1@gmail.com>
2020-05-07 18:44:53 +0545

    rendering list

---
+++ Conditionals and Rendering Lists/main.js
@@ -0,0 +1,17 @@
+new Vue({
+    el: '#app',
+    data:{
+        show: true,
+        ingredients: ['meat','fruit','cookies'],
+        persons: [
+            {name: 'Max',age: 27,color: 'red'},
+            {name: 'ana',age: 'unknown',color: 'blue'},
+        ]
+    },
+    computed:{
+
+    },
+    methods: {
+
+    }
+});
